06680a638a6049daec604a37b47b864ac1649edc
mindtripp <jaimealba1574@gmail.com>
2023-07-24 18:53:19 -0400

    new

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import users, recipes
+from flask_app.controllers import recipes, users
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
